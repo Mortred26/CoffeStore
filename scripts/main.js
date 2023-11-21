0c407e1c4febb97cab6a7e953b2bf5c38f87f8e7
Mortred26 <umarnurxodjayev29@gmail.com>
2023-11-21 13:39:58 +0500

    fix navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,17 @@
-$(document).ready(()=>
-{
-    $('.mean-toggle').click(()=>
-  {
-        $('.mean-toggle').toggleClass('active')
-        $('.nav-res').toggleClass('active')
-  })
-})
+$(document).ready(() => {
+  $(".mean-toggle").click(() => {
+    $(".mean-toggle").toggleClass("active");
+    $(".nav-res").toggleClass("active");
+  });
+});
+
+// function toggleMargin() {
+//   var myDiv = document.getElementById("myDiv");
+//   var currentMargin = parseInt(getComputedStyle(myDiv).marginTop);
+
+//   if (currentMargin === 0) {
+//     myDiv.style.marginTop = "100px";
+//   } else {
+//     myDiv.style.marginTop = "0";
+//   }
+// }
